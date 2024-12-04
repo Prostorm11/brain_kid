bf5b3efd2ca2c6260b36f118fac55fa26ee42a0e
Derrick Marfo <steammarfo111@gmail.com>
2024-12-04 14:13:35 +0000

    update2(ages_scroll_done)

--- lib/ProfileCreate/profile_screen.dart
+++ lib/ProfileCreate/profile_screen.dart
@@ -8,11 +8,39 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final ScrollController _scrollController = ScrollController();
+  late int _middleIndex = -1;
+  List<String> ages = ["", "", "1", "2", "3", "4", "5", "6", "", ""];
+
+  @override
+  void initState() {
+    _scrollController.addListener(setColor);
+    super.initState();
+  }
+
+  void setColor() {
+    double scrollCenter =
+        _scrollController.offset + MediaQuery.of(context).size.width / 2;
+
+    for (int i = 0; i < ages.length; i++) {
+      double itemStart = i * 75;
+      double itemEnd = itemStart + 60;
+
+      if (scrollCenter >= itemStart && scrollCenter <= itemEnd) {
+        setState(() {
+          _middleIndex = i;
+        });
+        break;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: SizedBox(
           width: double.infinity,
@@ -21,11 +49,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
             padding: const EdgeInsets.all(15),
             child: Column(
               children: [
-                Container(
-                  color: Colors.amber,
+                SizedBox(
                   height: height * 0.15,
                   child: Padding(
-                    padding: EdgeInsets.fromLTRB(10, 4, 10, 4),
+                    padding: const EdgeInsets.fromLTRB(10, 4, 10, 4),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
@@ -59,7 +86,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
                                   SizedBox(
                                     child: Image.asset("assets/Images/boy.png"),
                                   ),
-                                  const Text("Boy",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
+                                  const Text(
+                                    "Boy",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  )
                                 ],
                               ),
                             ),
@@ -69,14 +101,20 @@ class _ProfileScreenState extends State<ProfileScreen> {
                               decoration: BoxDecoration(
                                   color: const Color.fromRGBO(221, 35, 59, 1),
                                   borderRadius: BorderRadius.circular(20)),
-                                  child: Row(
+                              child: Row(
                                 mainAxisAlignment:
                                     MainAxisAlignment.spaceAround,
                                 children: [
                                   SizedBox(
-                                    child: Image.asset("assets/Images/girl.png"),
+                                    child:
+                                        Image.asset("assets/Images/girl.png"),
                                   ),
-                                  const Text("Girl",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
+                                  const Text(
+                                    "Girl",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  )
                                 ],
                               ),
                             )
@@ -86,19 +124,51 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     ),
                   ),
                 ),
-
-
-
-                Container(
-                  color: Colors.red,
-                  height: height * 0.55,
+                SizedBox(
+                  height: height * 0.60,
+                  child: Image.asset("assets/Images/pbackground.jpg"),
                 ),
-
-
-                
-                Container(
-                  color: Colors.orange,
-                  height: height * 0.2,
+                SizedBox(
+                  height: height * 0.17,
+                  child: Column(
+                    children: [
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Your Age",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      Expanded(
+                        // Make sure ListView has room to expand
+                        flex: 2,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          controller: _scrollController,
+                          itemCount: ages.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              margin: const EdgeInsets.all(5),
+                              width: 60,
+                              child: Center(
+                                child: Text(
+                                  ages[index],
+                                  style: TextStyle(
+                                    fontSize: _middleIndex == index ? 34 : 20,
+                                    fontWeight: FontWeight.w900,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {}, child: const Text("Continue"))
+                    ],
+                  ),
                 ),
               ],
             ),
