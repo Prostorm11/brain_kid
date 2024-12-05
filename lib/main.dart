ff6b162ade666484dad1042439a66563d4c79770
Derrick Marfo <steammarfo111@gmail.com>
2024-12-05 12:41:29 +0000

    Update3(homeScreen_done)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:brain_kid/ProfileCreate/profile_screen.dart';
+//import 'package:brain_kid/ProfileCreate/profile_screen.dart';
+import 'package:brain_kid/navigation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ProfileScreen()
+      home: const NavigateScreens()
     );
   }
 }
---
+++ lib/navigation.dart
@@ -0,0 +1,65 @@
+import 'package:brain_kid/Screens/homeScreen.dart';
+import 'package:brain_kid/Screens/setting.dart';
+import 'package:brain_kid/Screens/user_info.dart';
+import 'package:flutter/material.dart';
+
+class NavigateScreens extends StatefulWidget {
+  const NavigateScreens({super.key});
+
+  @override
+  State<NavigateScreens> createState() => _NavigateScreensState();
+}
+
+class _NavigateScreensState extends State<NavigateScreens> {
+  int _selectedIndex = 0;
+  static const List<Widget> screens = <Widget>[
+    Homescreen(),
+    Setting(),
+    UserInfo()
+  ];
+
+  void updateScreen(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: screens.elementAt(_selectedIndex)),
+      bottomNavigationBar: Container(
+        height: 100,
+        color: Colors.white,
+        margin: const EdgeInsets.only(bottom: 15),
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_outlined,
+                  size: 40,
+                ),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.settings,
+                  size: 40,
+                ),
+                label: "Setting"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  size: 40,
+                ),
+                label: "Personal"),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.red,
+          onTap: updateScreen,
+          elevation: 0,
+          enableFeedback: false,
+        ),
+      ),
+    );
+  }
+}
