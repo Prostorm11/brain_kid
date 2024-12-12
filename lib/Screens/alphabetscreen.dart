8ba26685b2d4c47bdacd1ffb07aeb435ea4b56f2
Derrick Marfo <steammarfo111@gmail.com>
2024-12-12 23:44:37 +0000

    update6(start_alphabet_screen)

---
+++ lib/Screens/alphabetscreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Alphabetscreen extends StatefulWidget {
+  const Alphabetscreen({super.key});
+
+  @override
+  State<Alphabetscreen> createState() => _AlphabetscreenState();
+}
+
+class _AlphabetscreenState extends State<Alphabetscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(child:  Scaffold(
+      body: SizedBox(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+
+    ));
+  }
+}
